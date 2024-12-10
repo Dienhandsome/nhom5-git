ba3cbe4010953c65130977960d4fdd7831d3e33d
BreadSC <ththien0223@gmail.com>
2024-12-10 08:37:10 +0700

    setup

---
+++ calculator/calculator/calculate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculator
+{
+	public partial class calculate : Form
+	{
+		public calculate()
+		{
+			InitializeComponent();
+		}
+
+		private void calculate_Load(object sender, EventArgs e)
+		{
+
+		}
+
+
+	}
+}
